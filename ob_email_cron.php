18ced144e7184d6a218d40e62e34fbc3290a7be6
Royce Harding <royce@empowernetwork.com>
2013-01-24 15:11:40 -0500

    add email logic

--- ob_email_cron.php
+++ ob_email_cron.php
@@ -1,63 +1,90 @@
 <?php
 
+/** Error reporting */
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+date_default_timezone_set('America/New_York');
+
+define('EOL',(PHP_SAPI == 'cli') ? PHP_EOL : '<br />');
+
 // get the configuration information
 require_once('./sites/default/settings.php');
 $url = parse_url($db_url);
 
-//echo '<pre>'; print_r($url); echo '</pre>';
+require_once('./functions.php');
 // open database connection
 $link = mysql_connect($url['host'], $url['user'], $url['pass']);
 if (!$link) {
+	echo mysql_error() . EOL;
+	error_log('ERROR - ob_email_cron.php Could not connect to database: ' . mysql_error());
     die('Could not connect: ' . mysql_error());
 }
 
-@mysql_select_db(str_replace('/', '', $url['path'])) or die( "Unable to select database");
-//echo 'Connected successfully';
+@mysql_select_db(str_replace('/', '', $url['path'])) or die("ERROR - ob_email_cron.php Unable to select database" . mysql_error());
+
 //read lmorse_drpl1 pcal_content_type_2date_registry table for entries with emails
-$sql = mysql_query("SELECT `field_pick_date_value`, `field_pick_date2_value`, `field_pick_date_choice_value`, `field_pick_date_email_value`, `field_pick_date_subscribe_check_value` FROM `pcal_content_type_2date_registry` WHERE `field_pick_date_email_value` IS NOT NULL AND `field_pick_date_subscribe_check_value` =  'Subscribe'");
+$sql = mysql_query(
+	'SELECT `registry`.`field_computed_week_value`, `registry`.`field_pick_date_email_value`, `node`.`timestamp`'
+	. '	FROM `pcal_content_type_2date_registry` AS `registry`,'
+	. '	`pcal_node_revisions` AS `node`'
+	. ' WHERE `registry`.`field_pick_date_email_value` IS NOT NULL'
+	. ' AND `registry`.`field_pick_date_subscribe_check_value` = ' . '"Subscribe"'
+	. ' AND `registry`.`nid` = `node`.`nid`'
+	. ' AND `registry`.`vid` = `node`.`vid`'
+	);
 $userinfo = array();
 
 while ($row_user = mysql_fetch_assoc($sql)) {
     $userinfo[] = $row_user;
 }
-//echo '<pre>'; print_r($userinfo); echo '</pre>';
+mysql_close($link);
 
 if (count($userinfo) > 0) {
 
 	$seconds_in_week = 604800;
-	$weeks_pregnant_39 = 40;
-	$now = strtotime("now");
+	$seconds_in_day = 86400;
+	$days_in_week = 7;
+	$weeks_pregnant_40 = 40;
+	$now = strtotime(date("M d Y"));
+	
+	$emails = getEmails();  // get the email templates
+	$post_script = '<br /><br />Visit us online at <a href="http://www.obgynspb.com" target="_blank">http://www.obgynspb.com</a> and like us on Facebook at <a href="http://www.facebook.com/obgynspecialists" target="_blank">http://www.facebook.com/obgynspecialists</a>.<br />==============================================<br /><br />To Unsubscribe, just reply to this email with the word Unsubscribe in the subject line.';
 
 	foreach ($userinfo AS $user) {
 	
-		//echo $user['field_pick_date_choice_value'] . '<br />';
+		$message = new stdClass;
+		
 		// calculate the number of weeks pregnant
-		switch ($user['field_pick_date_choice_value'])
-		{
-			case 'dday':
-				$date = substr($user['field_pick_date_value'], 0, 10);
-				$dday_time = strtotime($date);
-				$weeks_passed = ($dday_time - $now);
-				$weeks_till_dday = ($weeks_passed > 0) ? floor($weeks_passed / $seconds_in_week) : $weeks_pregnant_39 ;
-				$weeks_pregnant = ($weeks_till_dday < $weeks_pregnant_39) ? $weeks_pregnant_39 - $weeks_till_dday : $weeks_pregnant_39;
-				break;
-				
-			case 'mensday':
-				$date = substr($user['field_pick_date2_value'], 0, 10);
-				$mensday_time = strtotime($user['field_pick_date2_value']);
-				$weeks_passed = ($now - $mensday_time);
-				$weeks_pregnant = ($weeks_passed > 0) ? floor($weeks_passed / $seconds_in_week) : $weeks_pregnant_39 ;
-				break;
-				
-			default:
-				break;
+		$original_date = floor(date($user['timestamp'])/$seconds_in_week);
+		$now_date = floor(time()/$seconds_in_week);
+		$prego = $user['field_computed_week_value'];
+		$weeks_pregnant = $now_date - $original_date + $prego;
+		
+		//echo "EMail: {$user['field_pick_date_email_value']}, Number of weeks pregnant: $weeks_pregnant<br />";
+		$message->week = $weeks_pregnant;
+		$message->to = $user['field_pick_date_email_value'];
+		$message->email = 'Week Number ' . $weeks_pregnant . '<br /><br />' . $emails[$weeks_pregnant] . $post_script; // add the postscript to the message
+		
+		$error = false;
+			
+		if ((isset($message->week)) AND (isset($message->email)) AND (isset($message->to))) {
+			// email the message
+			$success = send_mail($message);
+			if (!($success)) {
+				$error = 'ERROR - Could not send email: ' . print_r($message, TRUE);
+			}
+		} else {
+			$error = 'ERROR - Could not get correct email values: ' . print_r($message, TRUE);
 		}
 		
+		if ($error) {
+			echo $error . EOL;
+			error_log($error);
+		}
 		
-		echo "EMail: {$user['field_pick_date_email_value']}, Selection:  {$user['field_pick_date_choice_value']}, Date: $date , Number of weeks pregnant: $weeks_pregnant<br />";
+		unset($message);
 	}
 }
 
-
 // send emails if appropriate
-mysql_close($link);
